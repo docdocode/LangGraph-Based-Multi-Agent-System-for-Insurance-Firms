c4dfca90934c4d2ce673c80f453b7d16148deb7a
docdocode <avirup.ov@gmail.com>
2025-02-10 10:03:13 +0800

    code cleaning for the knowledge base

--- index_knowledge_base.py
+++ index_knowledge_base.py
@@ -3,15 +3,12 @@ from langchain.vectorstores import Chroma
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.document_loaders import TextLoader
 
-# Load the knowledge base
 loader = TextLoader("data/faqs.txt")
 documents = loader.load()
 
-# Split the text into chunks
 text_splitter = CharacterTextSplitter(chunk_size=200, chunk_overlap=0)
 texts = text_splitter.split_documents(documents)
 
-# Generate embeddings and store in Chroma
 embeddings = OpenAIEmbeddings()
 vectorstore = Chroma.from_documents(texts, embeddings, persist_directory="chroma_db")
 vectorstore.persist()
